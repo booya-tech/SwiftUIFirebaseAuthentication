24d5615fc856942d9682bbe1b5d76052a9d05c4f
Boo Pannachai <booya.pannachai@gmail.com>
2025-01-10 15:05:17 +0700

    first commit:
    set up user profiles in firebase firestore (part9)

--- SwiftUIFirebaseBootcamp/Core/Authen/AuthenticationViewModel.swift
+++ SwiftUIFirebaseBootcamp/Core/Authen/AuthenticationViewModel.swift
@@ -9,20 +9,22 @@ import Foundation
 
 @MainActor
 final class AuthenticationViewModel: ObservableObject {
-    func signInGoogle() async throws {
+    func signInGoogle() async throws { 
         let helper = SignInGoogleHelper()
         let tokens = try await helper.signIn()
-        
-        try await AuthenticationManager.shared.signInWithGoogle(tokens: tokens)
+        let authDataResult = try await AuthenticationManager.shared.signInWithGoogle(tokens: tokens)
+        try await UserManager.shared.createNewUser(auth: authDataResult)
     }
     
     func signInApple() async throws {
         let helper = SignInAppleHelper()
         let tokens = try await helper.startSignInWithAppleFlowAsync()
-        try await AuthenticationManager.shared.signInWithApple(tokens: tokens)
+        let authDataResult = try await AuthenticationManager.shared.signInWithApple(tokens: tokens)
+        try await UserManager.shared.createNewUser(auth: authDataResult)
     }
 
     func signInAnonymous() async throws {
-        try await AuthenticationManager.shared.signInAnonymous()
+        let authDataResult = try await AuthenticationManager.shared.signInAnonymous()
+        try await UserManager.shared.createNewUser(auth: authDataResult)
     }
 }
